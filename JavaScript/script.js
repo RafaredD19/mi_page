a677eea4c2e03f688067f27df4ef45f7d7c1b89f
rafael <rosselgalarzarafael@gmail.com>
2024-01-13 20:29:32 -0500

    cambios de colores

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,17 +1,20 @@
+document.getElementById("icon_menu").addEventListener("click", toggleMenu);
 
-document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
-function mostrar_menu(){
+// Cierra el menú cuando se hace clic en un enlace del menú
+document.querySelectorAll(".nav__link").forEach(function(link) {
+    link.addEventListener("click", function() {
+        document.querySelector(".menu").classList.remove("mostrar_menu");
+    });
+});
+
+function toggleMenu() {
     document.querySelector(".menu").classList.toggle("mostrar_menu");
 }
 
-window.onscroll= function(){
-    var posicion =window.pageYOffset || document.documentElement.scrollTop;
+window.onscroll = function() {
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
     var elemento1 = document.getElementById("icon-hear");
-    var elemento2= document.getElementById("icon_fire");
-    elemento1.style.bottom= posicion * 0.3 + "px";
+    var elemento2 = document.getElementById("icon_fire");
+    elemento1.style.bottom = posicion * 0.3 + "px";
     elemento2.style.top = posicion * 0.3 + "px";
-}
-
-
-particlesJS("particles-js", {"particles":{"number":{"value":100,"density":{"enable":false,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"star","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"http://wiki.lexisnexis.com/academic/images/f/fb/Itunes_podcast_icon_300.jpg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":4,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":14,"direction":"left","random":false,"straight":true,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"window","events":{"onhover":{"enable":false,"mode":"grab"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":200,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
-
+};
